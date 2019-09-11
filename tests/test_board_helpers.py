10973065393b75fde824d3cb10c0ef476cce3a7e
lstrobel <lstrobel66@gmail.com>
2019-09-10 18:49:35 -0700

    Add tests for _find_unit_to_disband()

--- tests/test_board_helpers.py
+++ tests/test_board_helpers.py
@@ -17,10 +17,26 @@ import json
 
 import pytest
 
-from diplomacy import Board
+from diplomacy import Board, Order, Unit
 
 with open("/media/Storage/Projects/python-diplomacy/data/premade_boards/vanilla.json") as f:
     vanilla_board = Board(json.load(f))
+
+with open("/media/Storage/Projects/python-diplomacy/data/premade_boards/vanilla.json") as f:
+    test_board = Board(json.load(f))
+    orders = [Order("Russia", "Warsaw", "move", "Galicia")]
+    test_board.orders.extend(orders)
+    test_board.resolve_orders()
+    test_board.tiles[115].unit = Unit("France", "army")
+    test_board.tiles[89].unit = Unit("France", "army")
+    test_board.tiles[31].unit = Unit("France", "army")
+    test_board.tiles[86].unit = Unit("France", "army")
+    orders = [Order("Austria", "Vienna", "move", "Galicia"),
+              Order("Austria", "Budapest", "support", "Galicia", "Vienna")]
+    test_board.orders.extend(orders)
+    test_board.resolve_orders()
+    test_board.orders.append(Order("Russia", "Galicia", "retreat", "Ukraine"))
+    test_board.resolve_orders()
 
 
 @pytest.mark.parametrize('tile, player, distance', [(vanilla_board.tiles[29], "Germany", 0),
@@ -32,6 +48,17 @@ with open("/media/Storage/Projects/python-diplomacy/data/premade_boards/vanilla.
                                                     (vanilla_board.tiles[68], "Italy", 0),
                                                     (vanilla_board.tiles[68], "Turkey", 3),
                                                     (vanilla_board.tiles[13], "Turkey", 6),
-                                                    (vanilla_board.tiles[13], "Russia", 3)])
+                                                    (vanilla_board.tiles[13], "Russia", 3),
+                                                    (test_board.tiles[86], "France", 5)])
 def test__distance_to_home_center(tile, player, distance):
     assert vanilla_board._distance_to_home_center(tile, player) == distance
+
+
+@pytest.mark.parametrize('board, player, tile_id', [(test_board, "France", 86),
+                                                    (test_board, "Germany", 57),
+                                                    (test_board, "England", 46),
+                                                    (vanilla_board, "England", 46),
+                                                    (test_board, "Italy", 69),
+                                                    (vanilla_board, "Italy", 69)])
+def test__find_unit_to_disband(board, player, tile_id):
+    assert board._find_unit_to_disband(player).id == tile_id
